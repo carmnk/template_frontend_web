7cf7760a9b7d3d0b2ef3f8eaafbb7d016fb170d6
carmnk <carso.menk@gmail.com>
2024-07-24 15:28:44 +0200

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import pjson from './package.json'
+
+const homepage = pjson.homepage
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [(react as any)()],
@@ -10,4 +14,5 @@ export default defineConfig({
       external: ['@cmk/fe_utils'],
     },
   },
+  base: homepage,
 } as any)
