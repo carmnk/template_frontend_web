76b1313b85baaec85c3430dded0bf3d9642dde5e
carmnk <carso.menk@gmail.com>
2025-05-15 14:08:45 +0200

    0.1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-import { ElementModel, defaultEditorState } from "@cmk/fe_utils";
-import { BASE_ELEMENT_MODELS, deserializeEditorState } from "@cmk/fe_utils";
-import { AppHtmlRenderer } from "./AppHtmlRenderer";
-import axios from "axios";
-import packageJson from "../package.json";
+import React, { useEffect } from 'react'
+import { Toaster } from 'react-hot-toast'
+import { ElementModel, defaultEditorState } from '@cmk/fe_utils'
+import { BASE_ELEMENT_MODELS, deserializeEditorState } from '@cmk/fe_utils'
+import { AppHtmlRenderer } from './AppHtmlRenderer'
+import axios from 'axios'
+import packageJson from '../package.json'
 
 const prepareSerializesState = (appData: any) => {
   // console.log(
@@ -15,65 +14,65 @@ const prepareSerializesState = (appData: any) => {
   //   BASE_ELEMENT_MODELS
   // );
   console.log(
-    "response editorstate pre-prep",
+    'response editorstate pre-prep',
     appData,
     defaultEditorState(),
     BASE_ELEMENT_MODELS
     // transformedState
-  );
+  )
   const transformedState = deserializeEditorState(
     appData as any,
     defaultEditorState(),
     BASE_ELEMENT_MODELS as ElementModel[]
-  );
+  )
 
   return {
     ...transformedState,
     attributes: transformedState.attributes.map((attr: any) => {
       try {
         const attrValue =
-          attr.attr_name === "style" && typeof attr.attr_value === "string"
+          attr.attr_name === 'style' && typeof attr.attr_value === 'string'
             ? JSON.parse(attr.attr_value)
-            : attr.attr_value;
+            : attr.attr_value
         return {
           ...attr,
           attr_value: attrValue,
-        };
+        }
       } catch (e) {
-        console.error("error", e);
+        console.error('error', e)
       }
-      return attr;
+      return attr
     }),
-  };
-};
+  }
+}
 
 export const App = () => {
-  const [appData, setAppData] = React.useState<any>(null);
-  const [iconData, setIconData] = React.useState<Record<string, string>>({});
+  const [appData, setAppData] = React.useState<any>(null)
+  const [iconData, setIconData] = React.useState<Record<string, string>>({})
   useEffect(() => {
     const basePath =
-      packageJson.homepage && packageJson.homepage !== "/"
-        ? packageJson.homepage + "/"
-        : window.location.href ?? "/";
+      packageJson.homepage && packageJson.homepage !== '/'
+        ? packageJson.homepage + '/'
+        : window.location.href ?? '/'
 
     const fetchAppData = async () => {
       try {
-        console.log("fetching app data", window.location.href);
-        const url = `${basePath}app_data.json`;
-        const response = await axios.get(url);
-        const data = response.data;
+        console.log('fetching app data', window.location.href)
+        const url = `${basePath}app_data.json`
+        const response = await axios.get(url)
+        const data = response.data
         if (!data) {
-          throw new Error("No data found");
+          throw new Error('No data found')
         }
-        console.log("response editorstate", data);
-        const serializedState = prepareSerializesState(data);
-        console.log("response editorstate prepared", serializedState);
+        console.log('response editorstate', data)
+        const serializedState = prepareSerializesState(data)
+        console.log('response editorstate prepared', serializedState)
 
-        setAppData(serializedState);
+        setAppData(serializedState)
       } catch (e) {
-        console.error("error", e);
+        console.error('error', e)
       }
-    };
+    }
     // const fetchIconData = async () => {
     //   try {
     //     console.log("fetching icon data");
@@ -90,23 +89,19 @@ export const App = () => {
     //     console.error("error", e);
     //   }
     // };
-    fetchAppData();
+    fetchAppData()
     // fetchIconData();
-  }, []);
+  }, [])
 
   return (
     appData && (
       <>
-        <Routes>
-          <Route
-            path="/*"
-            element={<AppHtmlRenderer appData={appData} mdiIcons={iconData} />}
-          ></Route>
-        </Routes>
+        {/* Routing is done by the Renderer */}
+        <AppHtmlRenderer appData={appData} mdiIcons={iconData} />
         <Toaster />
       </>
     )
-  );
-};
+  )
+}
 
-export default App;
+export default App
